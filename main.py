1c1642aa3e6fa4664126d4fb3e89478103e0f89f
Derek Rhodes <physci@gmail.com>
2020-12-07 08:33:18 -0500

    just getting started

---
+++ main.py
@@ -0,0 +1,34 @@
+import calendar
+
+
+class Task:
+    def __init__(self):
+        pass
+
+class TaskManager:
+    """
+    task manager with priority queue
+    """
+    pass
+
+class Agenda:
+    """
+    build an agenda for a given day
+    """
+    pass
+
+class REPL:
+    def __init__(self):
+        pass
+
+    def go(self):
+        while 1:            
+            cmd = input(">> ")
+            print(f"doing {cmd}")
+            
+def main():
+    REPL().go()
+
+if __name__ == "__main__":
+    print("building agenda!")
+    main()
